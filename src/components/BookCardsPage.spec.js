56ebfa2e85d62f1f45a1681fd78f8e4098b1dbe8
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-03-10 20:23:10 +0100

    Add storybook and prepare tests

---
+++ src/components/BookCardsPage.spec.js
@@ -0,0 +1,10 @@
+import BookCardsPage from './BookCardsPage'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('BookCardsPage', () => {
+  it.todo('renders two filterbuttons and multiple bookcards by default')
+  it.todo(
+    'renders two favorited bookcards only when clicking the favorites button'
+  )
+})
---
+++ src/components/BookCardsPage.stories.js
@@ -0,0 +1,10 @@
+import BookCardsPage from './BookCardsPage'
+
+export default {
+  title: 'BookCardsPage',
+  component: BookCardsPage,
+}
+
+const Template = args => <BookCardsPage />
+
+export const Primary = Template.bind({})
