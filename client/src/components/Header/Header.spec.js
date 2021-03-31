24c3d8369768d055fbf750099ae4e61487d5ef5a
Eileen Fuerstenau <eileenfuerstenau@gmail.com>
2021-03-31 21:07:19 +0200

    Refine testing and add homepage testing

--- client/src/components/Header/Header.spec.js
+++ client/src/components/Header/Header.spec.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 
 describe('Header', () => {
   it('renders a header with the title given to it', () => {
-    render(<Header title="Lass dich inspirieren" />)
-    expect(screen.getByRole('heading').innerHTML).toBe('Lass dich inspirieren')
+    render(<Header>Lass dich inspirieren</Header>)
+    expect(screen.getByRole('banner').innerHTML).toBe('Lass dich inspirieren')
   })
 })
